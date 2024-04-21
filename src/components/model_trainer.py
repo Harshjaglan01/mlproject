e1b22776e6ccaa375a3c509fd51a69b8463689b9
Harsh Jaglan <harsh.j@cloudverse.ai>
2024-04-21 19:07:27 +0530

    prediction pipeline

--- src/components/model_trainer.py
+++ src/components/model_trainer.py
@@ -14,12 +14,10 @@ from sklearn.neighbors import KNeighborsRegressor
 from sklearn.tree import DecisionTreeRegressor
 from xgboost import XGBRegressor
 
-
 from src.exception import CustomException
 from src.logger import logging
 
 from src.utils import save_object,evaluate_models
-
 
 @dataclass
 class ModelTrainerConfig:
